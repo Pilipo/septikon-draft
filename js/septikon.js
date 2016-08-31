c00ecf85c6c872f31f05a4869cfda1f83801dc3f
Pilipo <pl@philliplehner.com>
2016-08-31 17:38:08 +0000

    Fixes previous break. Resources now build. Player can select Initial Clones equal to the amount of Oxygen in the Resources pool.

--- js/septikon.js
+++ js/septikon.js
@@ -1,233 +1,221 @@
-var Septikon = function(){
+var Septikon = Septikon || {}
 
+Septikon.directions = {N:1,E:2,S:4,W:8};	
+Septikon.playerPositions = {local:1, remote:2};
+Septikon.tileSize = 25;
+Septikon.tileGap = 4.89;
 
-	var directions = {N:1,E:2,S:4,W:8};	
-	var playerPositions;
-		var clone;
-		var board;
-		var group;
-		var tileSize = 25;
-		var tileCountX = 31;
-		var tileCountY = 21;
-		var tileGap = 4.89;
-		var playerPositions = {local:1, remote:2};
-
-}
 	
 Septikon.preload= function(game) {
-			game.load.image('board', 'assets/medium_board.png');
-			game.load.image('clone', 'assets/clone.png');
-		};
+	game.load.image('board', 'assets/medium_board.png');
+	game.load.image('clone', 'assets/clone.png');
+};
 		
-		Septikon.getLegalMoves= function(moves, currentCoord, previousCoord) {
-			moves--;
-			var legalMoves = [];
+Septikon.getLegalMoves= function(moves, currentCoord, previousCoord) {
+	moves--;
+	var legalMoves = [];
+	
+	dir={North:1,East:2,South:4,West:8};
+
+	for(direction in dir)
+	{	
+		//CHECK FOR LOCKS
+		
+		var moveCheck = this.getCoordFromDirection(currentCoord,direction);
+		
+		if(Septikon.checkWall(direction, currentCoord) === true &&  ((typeof previousCoord === 'undefined') || ((typeof previousCoord !== 'undefined') && (JSON.stringify(moveCheck) !== JSON.stringify(previousCoord))))) {
+			if(moves==0){
+				legalMoves.push(moveCheck);
+			}
+			else {
+				var returnArray = (Septikon.getLegalMoves(moves, moveCheck, currentCoord));
+				for(var i=0; i<returnArray.length; i++) {
+					if(JSON.stringify(returnArray[i]) !== JSON.stringify(currentCoord))
+						legalMoves.push(returnArray[i]);
+				}
+			}
+		}
+	}
+	return legalMoves;
+};
+		
+Septikon.create= function(game) {
+	this.group = game.add.group();
+	
+	Septikon.board = game.add.sprite(game.world.centerX, game.world.centerY, 'board');
+	Septikon.board.anchor.set(0.5);
+
+	this.player1 = new Septikon.Player("Player 1", "Red", Septikon.playerPositions.local);
+	this.player2 = new Septikon.Player("Player 2", "Blue",  Septikon.playerPositions.remote);
+
+	this.group.add(Septikon.board);
+	Septikon.tileStartX = Septikon.board.x - Septikon.board.width/2 + 52;
+	Septikon.tileStartY = Septikon.board.y - Septikon.board.height/2 + 51;
+
+	this.player1.initResources(game, this.player1);
+	//this.player2.initResources(game, this.player2);
+
+	this.group.pivot = {x:game.world.centerX,y:game.world.centerY};
+	this.group.x = game.world.centerX;
+	this.group.y = game.world.centerY;
+
+	this.createTiles(game);
+	
+	
+};
+		
+Septikon.createTiles= function(game){
+	
+	var graphics = game.add.graphics(0,0);
+	
+	var tileArray = [];
+	
+	var tileCountX = 31;
+	var tileCountY = 21;
+
+	
+	graphics.beginFill(0xFF3300);
+	graphics.lineStyle(1, 0xffd900, 1);
+	graphics.drawRoundedRect(0, 0, Septikon.tileSize, Septikon.tileSize, 2);
+	 
+	for(var tile_col = 0; tile_col < tileCountX; tile_col++)
+	{
+		for(var tile_row = 0; tile_row < tileCountY; tile_row++)
+		{
+			currentTile = game.add.sprite(Septikon.tileSize, Septikon.tileSize, graphics.generateTexture());
+			currentTile.anchor.set(0.5);
+			currentTile.title = "tile_" + tile_col + "_" + tile_row;
+			currentTile.x = Septikon.tileStartX + (Septikon.tileSize * tile_col) + (Septikon.tileGap * tile_col);
+			currentTile.y = Septikon.tileStartY + (Septikon.tileSize * tile_row) + (Septikon.tileGap * tile_row);
+			currentTile.alpha = 0.2;
 			
-			dir={North:1,East:2,South:4,West:8};
+			currentTile.inputEnabled = true;
+			currentTile.events.onInputDown.add(Septikon.listener, this);
+			
+			if(tile_col<9){
+				currentTile.tileOwner = Septikon.playerPositions.local;
+			}
+			
+			if(tile_col>22){
+				currentTile.tileOwner = Septikon.playerPositions.remote;
+			}
+			
+			
+			this.group.add(currentTile);
+			
+			if (typeof tileArray[tile_col] == 'undefined') 
+				tileArray[tile_col] = [];
+				
+			tileArray[tile_col][tile_row] = currentTile;
+			
+		}
+	}
 		
-			for(direction in dir)
-			{	
-				//CHECK FOR LOCKS
+	$(function () {
+		$.ajaxSetup({
+			cache:false
+		});
+		
+		$.getJSON( "js/tiles.json", function( data ) {
+			for(var key in data) {
+				if (!data.hasOwnProperty(key)) continue;
 				
-				var moveCheck = this.getCoordFromDirection(currentCoord,direction);
-				
-				if(Septikon.checkWall(direction, currentCoord) === true &&  ((typeof previousCoord === 'undefined') || ((typeof previousCoord !== 'undefined') && (JSON.stringify(moveCheck) !== JSON.stringify(previousCoord))))) {
-					if(moves==0){
-						legalMoves.push(moveCheck);
-					}
-					else {
-						var returnArray = (Septikon.getLegalMoves(moves, moveCheck, currentCoord));
-						for(var i=0; i<returnArray.length; i++) {
-							if(JSON.stringify(returnArray[i]) !== JSON.stringify(currentCoord))
-								legalMoves.push(returnArray[i]);
+				var obj = data[key];
+				for (var prop in obj) {
+					// skip loop if the property is from prototype
+					if(!obj.hasOwnProperty(prop)) continue;
+					
+					var locationCount = obj[prop].locations.length;
+					for( var i = 0; i < locationCount; i++) {
+						
+						var coords = obj[prop].locations[i].split(",");
+						var x = coords[0];
+						var y = coords[1];
+						
+						tileArray[x][y].xCoord = x;
+						tileArray[x][y].yCoord = y;
+
+						tileArray[x][y].tileType = obj[prop].type;
+
+						if (typeof tileArray[x][y] != 'undefined')
+							tileArray[x][y].tileName = obj[prop].name;
+						else
+							console.log(x + ":" + y + " not found!");
+						 
+						if (typeof obj[prop].properties != 'undefined') {
+							tileArray[x][y].tileResourceCostCount = obj[prop].properties.resourceCostCount;
+							tileArray[x][y].tileResourceCostType = obj[prop].properties.resourceCostType;
 						}
 					}
 				}
 			}
-			return legalMoves;
-		};
-		
-		Septikon.create= function(game) {
-		
-			this.group = game.add.group();
-			
-			this.board = game.add.sprite(game.world.centerX, game.world.centerY, 'board');
-			this.board.anchor.set(0.5);
-
-			this.player1 = new Septikon.Player("Player 1", "Red", Septikon.playerPositions.local);
-			this.player2 = new Septikon.Player("Player 2", "Blue",  Septikon.playerPositions.remote);
-
-			this.group.add(this.board);
-			this.player1.initResources(game, this.player1);
-			//this.player2.initResources(game, this.player2);
-
-			this.group.pivot = {x:game.world.centerX,y:game.world.centerY};
-			this.group.x = game.world.centerX;
-			this.group.y = game.world.centerY;
-			this.createTiles(game);
-		};
-		
-		Septikon.createTiles= function(game){
-			
-			var graphics = game.add.graphics(0,0);
-			
-			var tileArray = [];
-			
-			var tileSize = 25;
-			var tileCountX = 31;
-			var tileCountY = 21;
-			var tileGap = 4.89;
-			var tileStartX = Septikon.board.x - Septikon.board.width/2 + 52;
-			var tileStartY = Septikon.board.y - Septikon.board.height/2 + 51;
-
-			
-			graphics.beginFill(0xFF3300);
-			graphics.lineStyle(1, 0xffd900, 1);
-			graphics.drawRoundedRect(0, 0, tileSize, tileSize, 2);
-			 
-			for(var tile_col = 0; tile_col < tileCountX; tile_col++)
-			{
-				for(var tile_row = 0; tile_row < tileCountY; tile_row++)
-				{
-					currentTile = game.add.sprite(tileSize, tileSize, graphics.generateTexture());
-					currentTile.anchor.set(0.5);
-					currentTile.title = "tile_" + tile_col + "_" + tile_row;
-					currentTile.x = tileStartX + (tileSize * tile_col) + (tileGap * tile_col);
-					currentTile.y = tileStartY + (tileSize * tile_row) + (tileGap * tile_row);
-					currentTile.alpha = 0.2;
-					
-					currentTile.inputEnabled = true;
-					currentTile.events.onInputDown.add(Septikon.listener, this);
-					
-					if(tile_col<9){
-						currentTile.tileOwner = Septikon.playerPositions.local;
-					}
-					
-					if(tile_col>22){
-						currentTile.tileOwner = Septikon.playerPositions.remote;
-					}
-					
-					
-					this.group.add(currentTile);
-					
-					if (typeof tileArray[tile_col] == 'undefined') 
-						tileArray[tile_col] = [];
-						
-					tileArray[tile_col][tile_row] = currentTile;
-					
-				}
-			}
-				
-			$(function () {
-				$.ajaxSetup({
-					cache:false
-				});
-				
-				$.getJSON( "js/tiles.json", function( data ) {
-					for(var key in data) {
-						if (!data.hasOwnProperty(key)) continue;
-						
-						var obj = data[key];
-						for (var prop in obj) {
-							// skip loop if the property is from prototype
-							if(!obj.hasOwnProperty(prop)) continue;
-							
-							var locationCount = obj[prop].locations.length;
-							for( var i = 0; i < locationCount; i++) {
-								
-								var coords = obj[prop].locations[i].split(",");
-								var x = coords[0];
-								var y = coords[1];
-								
-								tileArray[x][y].xCoord = x;
-								tileArray[x][y].yCoord = y;
-
-								tileArray[x][y].tileType = obj[prop].type;
-
-								if (typeof tileArray[x][y] != 'undefined')
-									tileArray[x][y].tileName = obj[prop].name;
-								else
-									console.log(x + ":" + y + " not found!");
-								 
-								if (typeof obj[prop].properties != 'undefined') {
-									tileArray[x][y].tileResourceCostCount = obj[prop].properties.resourceCostCount;
-									tileArray[x][y].tileResourceCostType = obj[prop].properties.resourceCostType;
-								}
-							}
-						}
-					}
-				});
-			});
-			
-			graphics.destroy();
-			
-			return tileArray;
-		};
-		
-		
-		Septikon.listener= function (obj) {
-			this.player1.AddClone(obj);
-		};
-
-		Septikon.xCoordsToPixel= function (x) {
-			return Septikon.tileStartX + (x * (Septikon.tileSize+Septikon.tileGap));
-		};
-
-		Septikon.yCoordsToPixel= function (y) {
-			return Septikon.tileStartY + (y * (Septikon.tileSize+Septikon.tileGap));
-		};
-
-		Septikon.checkWall= function (direction, currentCoord) {
-		
-			var dir={North:1,East:2,South:4,West:8};
-			
-			switch (direction){
-				case "North": //UP
-					if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.North) == 0) {
-						return true;
-					}
-					break;
-				case "South": //DOWN
-					if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.South) == 0) {
-						return true;
-					}
-					break;
-				case "East": // right arrow key
-					if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.East) == 0) {
-						return true;
-					}
-					break;
-				case "West": // left arrow key
-					if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.West) == 0) {
-						return true;
-					}
-					break;
-				default:
-					return false;
-			}
-		};
-		
-		Septikon.rollDice= function(){
-			roll = Math.floor(Math.random() * 6) + 1;
-			console.log(Septikon.getLegalMoves(3,{x:1,y:10}));
-			return roll;  
-		};
-		
-		Septikon.getCoordFromDirection= function(originCoord,direction) {
-		
-			var dir={North:{"y":-1},East:{"x":1},South:{"y":1},West:{"x":-1}};
-			
-			if (typeof(dir[direction].y) !== "undefined")
-				return {x:originCoord.x, y: originCoord.y + parseInt(dir[direction].y)};
-				
-			if (typeof(dir[direction].x) !== "undefined")
-				return {x:originCoord.x + parseInt(dir[direction].x), y: originCoord.y}
-		};
+		});
+	});
 	
-
-Septikon.Game = function() {
+	graphics.destroy();
 	
-}
+	return tileArray;
+};
+		
+		
+Septikon.listener= function (obj) {
+	this.player1.AddClone(obj);
+};
+
+Septikon.xCoordsToPixel= function (x) {
+	return Septikon.tileStartX + (x * (Septikon.tileSize+Septikon.tileGap));
+};
+
+Septikon.yCoordsToPixel= function (y) {
+	return Septikon.tileStartY + (y * (Septikon.tileSize+Septikon.tileGap));
+};
+
+Septikon.checkWall= function (direction, currentCoord) {
+
+	var dir={North:1,East:2,South:4,West:8};
+	
+	switch (direction){
+		case "North": //UP
+			if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.North) == 0) {
+				return true;
+			}
+			break;
+		case "South": //DOWN
+			if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.South) == 0) {
+				return true;
+			}
+			break;
+		case "East": // right arrow key
+			if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.East) == 0) {
+				return true;
+			}
+			break;
+		case "West": // left arrow key
+			if (parseInt(WALL_GRID[currentCoord.x][currentCoord.y]&dir.West) == 0) {
+				return true;
+			}
+			break;
+		default:
+			return false;
+	}
+};
+
+Septikon.rollDice= function(){
+	roll = Math.floor(Math.random() * 6) + 1;
+	console.log(Septikon.getLegalMoves(3,{x:1,y:10}));
+	return roll;  
+};
+
+Septikon.getCoordFromDirection= function(originCoord,direction) {
+
+	var dir={North:{"y":-1},East:{"x":1},South:{"y":1},West:{"x":-1}};
+	
+	if (typeof(dir[direction].y) !== "undefined")
+		return {x:originCoord.x, y: originCoord.y + parseInt(dir[direction].y)};
+		
+	if (typeof(dir[direction].x) !== "undefined")
+		return {x:originCoord.x + parseInt(dir[direction].x), y: originCoord.y}
+};
 
 Septikon.Player = function(name, color, playerPosition) {
 	this.name = name;
